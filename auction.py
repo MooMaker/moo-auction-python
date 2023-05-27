bb550e7dbc33c4328ecd0c4889b7259ad81f120f
arjanjohan <arjanvanhoogdalem@gmail.com>
2023-05-27 08:54:09 +0200

    added some test cases

--- auction.py
+++ auction.py
@@ -2,22 +2,30 @@ class Auction:
 
     def __init__(self, json):
         self.json = json
-        self.id = json["auction_id"]
-        self.end_time = json["end_time"]
-        self.is_sell_order = True #TODO: Make logic for this
-        # TODO: we now assume it's 1 order per auction but this is not the case
-        #  create list of bids corresponding to order id's
+        self.id = json["metadata"]["auction_id"]
+        # self.end_time = json["end_time"]
+        # TODO: add end_time
+        self.is_sell_order = {}
         self.best_bid = {}
         for order_id in json["orders"]:
+            self.is_sell_order[int(order_id)] = json["orders"][order_id]["is_sell_order"];
             self.best_bid[int(order_id)] = {}
+
+    def parse_bids(self, market_maker_response):
+        for order_id in market_maker_response:
+            self.add_bid(int(order_id), market_maker_response[order_id])
+
 
     # TODO: Check with Mohammed (and marketmakers/cow) what format the bids will be in.
     #  Does amount_in/amount_out flip when sell order bool is true/false? We need to DOUBLE CHECK
-    def addBid(self, order_id, new_bid):
-        # TODO: Also here we need to match it to order_id
+    # TODO: There is no check on amountOut. Add this (just to verify it's equal)
+    def add_bid(self, order_id, new_bid):
+        best_bid = self.best_bid[order_id]
+        is_sell_order = self.is_sell_order[order_id]
+
         print("Adding bid")
-        if self.best_bid[order_id] == {} \
-        or (self.is_sell_order and new_bid.get("amountIn") > self.best_bid.get("amountIn")) \
-        or (not self.is_sell_order and new_bid.get("amountIn") < self.best_bid.get("amountIn")):
+        if best_bid == {} \
+        or (is_sell_order and new_bid.get("amountIn") > best_bid.get("amountIn")) \
+        or (not is_sell_order and new_bid.get("amountIn") < best_bid.get("amountIn")):
             self.best_bid[order_id] = new_bid
 
---
+++ tests/auction_test.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+from auction import Auction
+
+def test_single_order_single_response():
+    cow_file = open('../examples/single_order_instance1_input.json')
+    order_json = json.load(cow_file)
+    auction = Auction(order_json)
+
+    mm_file = open('../examples/mm_response_1.json')
+    mm_order = json.load(mm_file)
+    auction.parse_bids(mm_order)
+    assert mm_order["0"] == auction.best_bid[0]
+
+def test_single_order_multiple_response():
+    cow_file = open('../examples/single_order_instance1_input.json')
+    order_json = json.load(cow_file)
+    auction = Auction(order_json)
+
+    mm_file_paths = ['../examples/mm_response_1.json',
+                '../examples/mm_response_2.json',
+                '../examples/mm_response_3.json']
+    mm_orders = []
+    for mm_file_path in mm_file_paths:
+        mm_file = open(mm_file_path)
+        mm_orders.append(json.load(mm_file))
+        auction.parse_bids(mm_orders[-1])
+    assert mm_orders[1]["0"] == auction.best_bid[0]
